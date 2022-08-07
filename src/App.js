de57b917a0d9309285645a437d65658b71cd4219
gajeraankil <gajeraankil@gmail.com>
2022-08-07 18:58:44 +0530

    redux-persist

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Counter from "./component/Counter";
 import { Provider } from "react-redux";
 import { configureStore } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
-  const store = configureStore();
+  const { store, persistor } = configureStore();
   return (
     <>
       <Provider store={store}>
-        <Counter />
+        <PersistGate loading={null} persistor={persistor}>
+          <Counter />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import { rootReducer } from "./reducer";
 
-export const configureStore = () => {
-  let store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ['counter']
+};
 
-  return store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => {
+  let store = createStore(persistedReducer, applyMiddleware(thunk));
+  let persistor = persistStore(store);
+
+  return { store, persistor };
 };
